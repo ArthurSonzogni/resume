49e6470c3a7f587b5e1dc0e1300bde05cfa67cc5
ArthurSonzogni <sonzogniarthur@gmail.com>
2025-06-10 16:13:33 +0200

    Update package + add (present)

--- components/SubElement.vue
+++ components/SubElement.vue
@@ -15,11 +15,15 @@ const startDateSimple = computed(() => {
 
 const endDateSimple = computed(() => {
   // If the end date is close to "today", we display "present" instead of the date.
-  if (props.endDate > new Date() && props.endDate - new Date() < 1000 * 60 * 60 * 24 * 30) {
-    return 'Present'
-  }
+  const present = (
+    props.endDate.getFullYear() === new Date().getFullYear() &&
+    props.endDate.getMonth() === new Date().getMonth()
+  );
 
-  return props.endDate.toLocaleDateString('en-US', { month: 'long', year: 'numeric' })
+  return props.endDate.toLocaleDateString('en-US', {
+    month: 'long',
+    year: 'numeric'
+  }) + (present ? ' (present)' : '');
 })
 
 const duration = computed(() => {
